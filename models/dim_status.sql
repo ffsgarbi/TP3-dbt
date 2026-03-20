9e7adb46f36e412730b49666a32e5d0ca743dd19
Felipe Sgarbi <felipesgarbi14@gmail.com>
2026-03-20 00:34:42 +0000

    todos completos

--- models/dim_status.sql
+++ models/dim_status.sql
@@ -3,7 +3,7 @@ with orders as (
 )
 
 select distinct
-    {{ dbt_utils.generate_surrogate_key(['status']) }} as status_id,
-    status                                             as order_status
+    md5(status)    as status_id,
+    status         as order_status
 from orders
 where status is not null
