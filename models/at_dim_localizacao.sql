75842cca72a892d6044a44e010a9f31ef6a687ca
Felipe Sgarbi <felipesgarbi14@gmail.com>
2026-03-27 01:58:52 +0000

    dim e fato criadas

---
+++ models/at_dim_localizacao.sql
@@ -0,0 +1,10 @@
+with customers as (
+    select * from {{ ref('at_stg_customers') }}
+)
+
+select distinct
+    md5(cep_prefixo || cidade || estado)    as location_id,
+    cep_prefixo,
+    cidade,
+    estado
+from customers
